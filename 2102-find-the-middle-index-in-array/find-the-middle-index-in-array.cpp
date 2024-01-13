3a2e7ca71d9a5ea0df9715bcb3f7623d13215a57
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-01-13 21:58:16 +0530

    Time: 4 ms (46.34%) | Memory: 12.7 MB (66.22%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int s=0;
+        for(int i=0;i<nums.size();i++){
+            s+=nums[i];
+        }
+        int prefix=0;
+        for(int i=0;i<nums.size();i++){
+            prefix+=nums[i];
+            if((s-prefix)==(prefix-nums[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
