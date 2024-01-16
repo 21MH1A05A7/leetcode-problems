26e2dd1bc218a3cc82ee58456d31ff6f598c9f18
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-01-17 00:17:00 +0530

    Time: 30 ms (57.21%) | Memory: 28.5 MB (56.45%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // int i=0;
+        // int j=0;
+        // // int s=nums[i];
+        // int s=0;
+        // int mi=INT_MAX;
+        // while(j<nums.size()){
+        //     if(s>=target){
+        //         s=s-nums[i];
+        //         cout<<i<<" "<<j<<endl;
+        //         mi=min(j-i+1,mi);
+        //         i=i+1;
+        //     }
+        //     else if(s<target){
+        //         s+=nums[j];
+        //         j+=1;
+        //     }
+            
+        // }
+        // // cout<<i<<" "<<j<<endl;
+        //     while(i<j){
+        //         if(s<target){
+        //             break;
+        //         }
+        //         else if(s>=target){
+        //             // cout<<s<<endl;
+        //             s=s-nums[i];
+                    
+        //             mi=min(j-i+1,mi);
+        //             cout<<i<<" "<<j<<endl;
+        //             i+=1;
+        //         }
+        //     }
+
+        // if(mi==INT_MAX){
+        //     return 0;
+        // }
+        // return mi;
+
+        long long i = 0, j = 0, sum = 0, mini = LLONG_MAX;
+
+        while (j < nums.size ())
+        {
+            if ((nums [j] + sum) < target)
+                sum += nums [j++];
+            else
+            {
+                mini = min (mini, abs (j - i + 1));
+                sum -= nums [i++];
+            }
+        }
+        if (mini == LLONG_MAX)
+            return 0;
+    
+        return mini;
+
+    }
+};
