d47fa7f2697a2fa81cc2eb33e42a22648fc63622
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-01-27 21:58:26 +0530

    Time: 53 ms (5.44%) | Memory: 60.5 MB (5.02%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> v1;
+    void combination(int ind,vector<int> arr,int s,vector<int>d,int n){
+        if(s==0){
+            // sort(d.begin(),d.end());
+            v1.push_back(d);
+            return;    
+        }
+    else{
+        // s-=arr[ind];
+        for(int i=ind;i<n;i++){
+            // d.push_back(arr[ind]);
+            if(i>ind && arr[i]==arr[i-1]){
+                continue;
+            }
+            if(s>0){
+                d.push_back(arr[i]);
+                combination(i+1,arr,s-arr[i],d,n);
+                d.pop_back();
+            }
+            else break;
+        }
+     }
+    
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> d;
+        sort(candidates.begin(),candidates.end());
+        combination(0,candidates,target,d,candidates.size());
+        vector<vector<int>> res;
+        
+        for(auto ele:v1){
+            res.push_back(ele);
+        }
+        return res;
+    }
+};
