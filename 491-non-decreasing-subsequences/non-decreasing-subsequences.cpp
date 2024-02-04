430fae176f39023160c9aefc2c96e35557a88246
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-02-04 11:29:38 +0530

    Time: 50 ms (68.10%) | Memory: 27.9 MB (48.43%) - LeetSync

---
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void func(int ind,vector<int>& nums,set<vector<int>>& res,int n,vector<int>& ans){
+        if(ind==n){
+            if(ans.size()>1){
+                res.insert(ans);
+            }
+            
+            return;
+        }
+        
+        if(ans.size()==0 || ans[ans.size()-1]<=nums[ind]){
+                ans.push_back(nums[ind]);
+                func(ind+1,nums,res,n,ans);
+                ans.pop_back();
+            }
+        func(ind+1,nums,res,n,ans);
+
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+        set<vector<int>> res;
+        func(0,nums,res,n,ans);
+        vector<vector<int>> result;
+        for(auto ele:res){
+            result.push_back(ele);
+        }
+        return result;
+    }
+};
