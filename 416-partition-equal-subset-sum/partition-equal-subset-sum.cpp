9bdc8cba2106afd3e0f52671c685865f67cfbba8
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-02-18 21:03:37 +0530

    Time: 656 ms (10.04%) | Memory: 55.9 MB (52.69%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dp[1001][10000];
+    bool func(int ind,int s,vector<int>&nums,int total){
+        if(s>(total-s)){
+            return 0;
+        }
+        if(s==(total-s)){
+            return 1;
+        }
+        if(dp[ind][s]!=-1){return dp[ind][s];}
+        if(ind==nums.size()){
+            if(s==(total-s)){
+                return 1;
+            }
+            return 0;
+        }
+        
+        bool pick=func(ind+1,s+nums[ind],nums,total);
+        bool npick=func(ind+1,s,nums,total);
+        return dp[ind][s]=pick||npick;
+    }
+
+
+
+    bool canPartition(vector<int>& nums) {
+        int total=0;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<nums.size();i++){
+            total+=nums[i];
+        }
+        return func(0,0,nums,total);
+    }
+};
