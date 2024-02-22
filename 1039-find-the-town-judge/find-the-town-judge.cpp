e522fa5ab4c962fae4ce72d53ce6030559b5c141
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-02-22 10:38:00 +0530

    Time: 129 ms (74.29%) | Memory: 66.2 MB (48.10%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        map<int,int> s; //second elements
+        map<int,int> st; //first elements
+        if(trust.size()==0){
+            if(n==1){
+                return n;
+            }
+            else{
+                return -1;
+            }
+        }
+
+        for(int i=0;i<trust.size();i++){
+            s[trust[i][1]]++;
+            st[trust[i][0]]++;
+        }
+        int as=0;
+        for(auto ele:st){
+            as+=ele.first;
+        }
+        int su=(n*(n+1))/2;
+        int len=st.size();
+        for(auto ele:s){
+            if(!st[ele.first]){
+                if((ele.first*ele.second)==((n-1)*ele.first)){
+                   return ele.first;
+                }
+                
+            }
+        }
+        return -1;
+    }
+};
