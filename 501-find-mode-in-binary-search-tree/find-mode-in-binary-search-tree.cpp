3f7211fa9ecf567d3de5d515f2ff090bd1a4b062
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-02-28 18:25:40 +0530

    Time: 24 ms (17.90%) | Memory: 23.9 MB (71.67%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // set<int> s;
+    // void func1(TreeNode* root){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     s.insert(root->val);
+    //     func1(root->left);
+    //     func1(root->right);
+    // }
+    map<int,int> mp;
+    int ma=0;
+    void func(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        func(root->left);
+        mp[root->val]++;
+        ma=max(ma,mp[root->val]);
+        func(root->right);
+    }
+    vector<int> findMode(TreeNode* root) {
+        func(root);
+        vector<int> v1;
+        for(auto ele:mp){
+            if(ele.second==ma){
+                v1.push_back(ele.first);
+            }
+        }
+        return v1;
+    }
+};
