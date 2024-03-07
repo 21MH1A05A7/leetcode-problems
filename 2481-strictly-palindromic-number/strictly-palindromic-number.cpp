1a62ba33d1e79f67c99c31f39fee22c23fb612a9
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-03-07 21:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (6.37%) - LeetSync

---
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool palin(string s){
+        string t;
+        for(int i=0;i<s.size();i++){
+            t=s[i]+t;
+        }
+        if(t==s){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+
+
+    bool isStrictlyPalindromic(int t) {
+        // for(int i=2;i<=t-2;i++){
+            int n=t;
+            string s;
+
+            int i=t-2;
+            while(n!=1){
+                int rem=n%i;
+                s=to_string(rem)+s;
+                n=n/i;
+            }
+            s='1'+s;
+            // cout<<s<<" ";
+            if(!palin(s)){
+                return 0;
+            }
+        // }
+        return 1;
+    }
+};
