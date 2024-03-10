de1920d06a5471b95211a7e67026a9846e7663b5
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2024-03-10 21:31:29 +0530

    Time: 253 ms (9.74%) | Memory: 20.8 MB (6.66%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int func(int ind,int n,vector<int>& nums,vector<int>& dp){
+        if(ind>=n-1){
+            return 0;
+        }
+        if(nums[ind]==0){return 1e9;}
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int ans=INT_MAX;
+        for(int i=1;i<=nums[ind];i++){
+            ans=min(ans,1+func(ind+i,n,nums,dp));
+        }
+        return dp[ind]=ans;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return func(0,n,nums,dp);   
+    }
+};
